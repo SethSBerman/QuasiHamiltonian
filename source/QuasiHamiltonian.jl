b28ab9cab69ecf6832e4513c3a94d2c7d223ca99
Seth Berman <bermanseth1@gmail.com>
2020-08-19 03:16:38 -0600

    Made it work somewhat

--- source/QuasiHamiltonian.jl
+++ source/QuasiHamiltonian.jl
@@ -1,30 +1,46 @@
 using LinearAlgebra
+using ForwardDiff
 
-E = Complex[0 1;0 0]
-F = Complex[0 0;1 0]
-H = Complex[1 0;0 -1]
+workingDim = 3
+Eexp(t::Real) = [1 t;0 1]
+Fexp(t::Real) = [1 0;t 1]
+Hexp(t::Real) = [exp(t) 0;0 exp(-t)]
 
-function rep(A)
+function rep(A::Array{<:Real})
     a=A[1,1]
     b=A[1,2]
     c=A[2,1]
     d=A[2,2]
-    return Complex[a^2 a*b b^2; 2*a*c (c*b)+(a*d) 2*b*d; c^2 c*d d^2]
+    return Real[a^2 a*b b^2; 2*a*c (c*b)+(a*d) 2*b*d; c^2 c*d d^2]
 end
 
-function funcMatrix(A)
-    return Complex[rep(A) zeros(Complex, 3, 3);zeros(Complex, 3, 3) transpose(rep(inv(A)))]
+function funcMatrix(A::Array{<:Real})
+    return Real[rep(A) zeros(Real, 3, 3);zeros(Real, 3, 3) transpose(rep(inv(A)))]
 end
 
-function scalarEx(B)
-    exponentFunc = function(t::Complex) return exp(t*B) end
-    return exponentFunc
+function getValue(j, k)
+    f(A) = A[j,k]
+    return f
 end
 
-function pointTangent(x1, x2, x3, y1, y2, y3, K)
-    timesPoint = function(D) return D*Complex[x1; x2; x3; y1; y2; y3] end
-    return #= derivative =# (timesPoint∘(funcMatrix∘(scalarEx(K))))
+function pointTangent(x1::Real, x2::Real, x3::Real, y1::Real, y2::Real, y3::Real, K)
+    A = Array{Int64}(undef, (2*workingDim,2*workingDim))
+        for i = 1:2*workingDim
+            for j = 1:2*workingDim
+                g = t -> (getValue(i,j)∘(funcMatrix∘K))(t)
+                h = ForwardDiff.derivative(g, 0)
+                A[i,j]=h
+            end
+        end
+    return A*[x1; x2; x3; y1; y2; y3]
 end
+
+function infAction(a::Real,b::Real,c::Real)
+    vec(x1::Real, x2::Real, x3::Real, y1::Real, y2::Real, y3::Real) = b*pointTangent(x1, x2, x3, y1, y2, y3, Eexp) + c*pointTangent(x1, x2, x3, y1, y2, y3, Fexp) + a*pointTangent(x1, x2, x3, y1, y2, y3, Hexp)
+    return vec
+end
+
+
 
 
 
